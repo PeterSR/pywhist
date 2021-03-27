acf71705299022586175a070250ae9ffd003a809
Peter Severin Rasmussen <git@petersr.com>
2021-03-27 13:49:55 +0100

    Updated package info (setup.py) before publishing.

--- setup.py
+++ setup.py
@@ -2,21 +2,19 @@
 
 from setuptools import setup, find_packages
 
-
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
-    name='whist',
-    version='0.1.0',
-    description='A package containing the logic for the card game Whist and its variants.',
+    name="whist",
+    version="0.1.0",
+    description="A package containing the logic for the card game Whist and its variants.",
     long_description=readme,
-    author='Peter Severin Rasmussen',
-    author_email='git@petersr.com',
-    url='https://github.com/PeterSR/pywhist',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    long_description_content_type="text/markdown",
+    author="Peter Severin Rasmussen",
+    author_email="git@petersr.com",
+    url="https://github.com/PeterSR/pywhist",
+    license="MIT License",
+    packages=find_packages(exclude=("tests", "docs")),
+    python_requires=">=3.7",
 )
